4411a24c5dff40edff5ff5e2e08d441049fa75ac
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-09 09:50:09 +0200

    add

--- Script/code.js
+++ Script/code.js
@@ -1,4 +1,19 @@
-let numb1 = 1
-let numb2 = 2
+// function addition(){
+//     console.log()
+// }
 
-console.log(numb1 + numb2);
+// const btn = document.getElementById('btnAddition')
+// btnAdd.addEventListener('click' , addition)
+
+
+const btnAdd = document.querySelector('[data-submit]')
+const lblOutput = document.querySelector('#output')
+
+function addition(){
+    let numb1 = document.getElementById('numb1').value
+    let numb2 = document.getElementById('numb2').value
+    let sum = +numb1 + +numb2
+    lblOutput.textContent = sum
+}
+
+btnAdd.addEventListener('click', addition)
